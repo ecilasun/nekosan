567cbfb2d4d69dfd9afe73ae15ee1e64620a5671
Engin Cilasun <ecilasun@me.com>
2021-08-20 01:59:51 -0400

    Added division by zero case to integer divide Changed optimizations to hit 0.928ns WNS

--- nekosan.srcs/sources_1/new/dividers.sv
+++ nekosan.srcs/sources_1/new/dividers.sv
@@ -22,22 +22,29 @@ wire [32:0] sub_add=r_sign?({reg_r,quotient[31]}+{1'b0,reg_b}):
 assign remainder = r_sign ? reg_r + reg_b : reg_r;
 assign quotient = reg_q;
 
-always @(posedge clk or posedge reset)begin
-    if(reset)begin
+always @(posedge clk or posedge reset) begin
+    if(reset) begin
         //count<=0;
         busy<=0;
         busy2<=0;
     end else begin
         busy2<=busy;
-        if(start)begin
-            reg_r<=32'b0;
-            r_sign<=0;
-            reg_q<=dividend;
-            reg_b<=divisor;
-            count<=0;
-            busy<=1;
+        if(start) begin
+        	if (divisor==0) begin
+        		// Divide by zero
+        		reg_q <= 32'hFFFFFFFF;
+        		reg_r <= dividend;
+        		busy<=0;
+        	end else begin
+				reg_r<=32'b0;
+				r_sign<=0;
+				reg_q<=dividend;
+				reg_b<=divisor;
+				count<=0;
+				busy<=1;
+            end
         end
-        else if(busy)begin
+        else if(busy) begin
             reg_r<=sub_add[31:0];
             r_sign<=sub_add[32];
             reg_q<={reg_q[30:0],~sub_add[32]};
@@ -74,29 +81,35 @@ assign reg_r2 = r_sign ? reg_r + reg_b : reg_r;
 assign remainder = dividend[31]?(~reg_r2+1):reg_r2;
 assign quotient = (divisor[31]^dividend[31])?(~reg_q+1):reg_q;
 
-always @(posedge clk or posedge reset)begin
-    if(reset)begin
+always @(posedge clk or posedge reset) begin
+    if(reset) begin
         //count<=0;
         busy<=0;
         busy2<=0;
-    end
-    else begin
+    end else begin
         busy2<=busy;
-        if(start)begin
-            reg_r<=32'b0;
-            r_sign<=0;
-            if(dividend[31]==1) begin
-                reg_q<=~dividend+1;
-            end
-            else reg_q<=dividend;
-            if(divisor[31]==1)begin
-                reg_b<=~divisor+1;
-            end
-            else reg_b<=divisor;
-            count<=0;
-            busy<=1;
+        if(start) begin
+        	r_sign<=0;
+        	if (divisor==0) begin
+        		// Divide by zero
+        		reg_q <= 32'hFFFFFFFF;
+        		reg_r <= dividend;
+        		busy<=0;
+			end else begin
+				reg_r<=32'b0;
+				if(dividend[31]==1) begin
+					reg_q<=~dividend+1;
+				end
+				else reg_q<=dividend;
+				if(divisor[31]==1)begin
+					reg_b<=~divisor+1;
+				end
+				else reg_b<=divisor;
+				count<=0;
+				busy<=1;
+			end
         end
-        else if(busy)begin
+        else if(busy) begin
             reg_r<=sub_add[31:0];
             r_sign<=sub_add[32];
             reg_q<={reg_q[30:0],~sub_add[32]};
